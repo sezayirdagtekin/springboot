a94a252e7ddc507498c22e7910aa60287f35d410
Sezayir Dağtekin <sezayir@hotmail.com>
2022-02-05 18:59:39 +0300

    refactor

--- HotelReservation/src/main/java/com/sezo/demo/service/ReservationService.java
+++ HotelReservation/src/main/java/com/sezo/demo/service/ReservationService.java
@@ -2,7 +2,6 @@ package com.sezo.demo.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,15 +33,31 @@ public class ReservationService {
 	}
 
 	public List<RoomReservation> getRoomReservationsForDate(LocalDate date) {
-		Iterable<Room> rooms = this.roomRepository.findAll();
-		Map<Long, RoomReservation> roomReservationMap = new HashMap<Long, RoomReservation>();
-		rooms.forEach(room -> {
-			RoomReservation roomReservation = new RoomReservation();
-			roomReservation.setRoomId(room.getId());
-			roomReservation.setRoomName(room.getName());
-			roomReservation.setRoomNumber(room.getRoomNumber());
-			roomReservationMap.put(room.getId(), roomReservation);
+		Map<Long, RoomReservation> roomReservationMap = getRoomReservationMap();
+
+		getReservationByDate(date, roomReservationMap);
+
+		List<RoomReservation> roomReservations = sortReservartion(roomReservationMap);
+		return roomReservations;
+	}
+
+	private List<RoomReservation> sortReservartion(Map<Long, RoomReservation> roomReservationMap) {
+		List<RoomReservation> roomReservations = new ArrayList<>();
+		roomReservationMap
+		.entrySet()
+		.stream()
+		.forEach(e -> roomReservations.add(e.getValue()));
+
+		roomReservations.sort((RoomReservation o1, RoomReservation o2) -> {
+			if (o1.getRoomName().equals(o2.getRoomName())) {
+				return o1.getRoomNumber().compareTo(o2.getRoomNumber());
+			}
+			return o1.getRoomName().compareTo(o2.getRoomName());
 		});
+		return roomReservations;
+	}
+
+	private void getReservationByDate(LocalDate date, Map<Long, RoomReservation> roomReservationMap) {
 		Iterable<Reservation> reservations = this.reservationRepository.findReservationByReservationDate(date);
 		reservations.forEach(reservation -> {
 			RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
@@ -52,21 +67,18 @@ public class ReservationService {
 			roomReservation.setLastName(guest.getLastName());
 			roomReservation.setGuestId(guest.getGuestId());
 		});
-		List<RoomReservation> roomReservations = new ArrayList<>();
-		for (Long id : roomReservationMap.keySet()) {
-			roomReservations.add(roomReservationMap.get(id));
-		}
-		roomReservations.sort(new Comparator<RoomReservation>() {
-			@Override
-			public int compare(RoomReservation o1, RoomReservation o2) {
-				if (o1.getRoomName().equals(o2.getRoomName())) {
-					return o1.getRoomNumber().compareTo(o2.getRoomNumber());
-				}
-				return o1.getRoomName().compareTo(o2.getRoomName());
-			}
+	}
+
+	private Map<Long, RoomReservation> getRoomReservationMap() {
+		Iterable<Room> rooms = this.roomRepository.findAll();
+		Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+		rooms.forEach(room -> {
+			RoomReservation roomReservation = new RoomReservation();
+			roomReservation.setRoomId(room.getId());
+			roomReservation.setRoomName(room.getName());
+			roomReservation.setRoomNumber(room.getRoomNumber());
+			roomReservationMap.put(room.getId(), roomReservation);
 		});
-		return roomReservations;
+		return roomReservationMap;
 	}
 }
-
-
